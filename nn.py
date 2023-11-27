6b0b717d2a1d00ca70241a7e0eeac2fcfada06b6
Will M <bennuashes@gmail.com>
2023-11-26 22:45:55 -0500

    fully working, 60-90% success rate

--- nn.py
+++ nn.py
@@ -4,19 +4,24 @@ import numpy as np
 import os, glob
 from PIL import Image
 import sys
-# np.set_printoptions(threshold=sys.maxsize) 
+np.set_printoptions(threshold=sys.maxsize) 
 def main():
     
     # m = number of test cases that have been added
-    trainingPath = "../trainingdata/Reduced MNIST Data/Reduced Trainging data"
+    # trainingPath = "../trainingdata/Reduced MNIST Data/Reduced Trainging data"
     # testingPath = "../trainingdata/Reduced MNIST Data/Reduced Testing data"
+    trainingPath = "../trainingdata/MNIST Data/Reduced Trainging data"
+    testingPath = "../trainingdata/MNIST Data/Reduced Testing data"
+    
     trainingPaths = get_one_pathlist(trainingPath)
-    # originalTrainingPaths = trainingPaths.copy()
-    # np.random.shuffle(trainingPaths)
+    originalTrainingPaths = trainingPaths.copy()
+    testingPaths = get_one_pathlist(testingPath)
+    np.random.shuffle(trainingPaths)
+    np.random.shuffle(testingPaths)
     # trainingPaths.reverse()
     # training, testing = get_dataset()
-    sc = np.sqrt(2/784)
-    sc2 = np.sqrt(2/10)
+    sc = 2/784
+    sc2 = 2/10
     W1 = np.random.normal(loc=0, scale=sc,size=(10,784)) # (low=.0001,high=.5,size=(10,784))
     W2 = np.random.normal(loc=0, scale=sc2,size=(10,10)) # randint(low=1, high=10, size=(10,10)) # (loc=0, scale=sc2,size=(10,10)) # (low=.0001, high=.5,size=(10,10))
     
@@ -30,64 +35,136 @@ def main():
     
     X = []
     Y = []
-    c = 0
-    while len(nextPath) > 0:
-        
-        if c > 100:
-            break
+    c = -1
+    print("W avgs")
+    # startingTrainingCount = len(originalTrainingPaths)
+    print(np.average(W1))
+    print(np.average(W2))
+
+    while len(nextPath) > 0: # and c <90:
         c += 1
+
+        MAX_INPUT = 255 # 0 to 255 per pixel
         nextTarget = get_parent_path(nextPath)
-        nextDataset = path_to_pixels(nextPath)
-        # .001 every 100 can adjust a whole cycle,  .1 = every 10, .2 every 5
-        
+        nextDataset = path_to_pixels(nextPath)/(MAX_INPUT/10) # gives us a range between 0 and 10
+
         # Y is subtracted from the final probabilities
+        # What we're solving for is Y hat or the Y vector of certainy (probability)
         Y.append(fill_at(int(nextTarget)))
         X.append(nextDataset)
+    
+        X,Y,W1,W2,b1,b2,A0,A1,A2,Z1,Z2 = there_and_back_again(X,Y,W1,W2,b1,b2,learningRate)
         
-        # number is the layer. 0 = input, 1 = hidden, 2 = output
-        # A is the input set, b is the bias
-        # W are weights that are modified by the forward and backward propagation
-        # print()
-        A0 = np.transpose(X) # m x 784 -> 784 x m
-
-        
-        # train the system
-        # A2 is a list of probabilities, 10 total, one for each digit. These total up to 1.
-        Z1, Z2, A1, A2 = forward_propagation(A0, W1, W2, b1, b2)
-        
-        
-        # take what we learned from the training and apply it to our weights so we can learn
-        W1, W2, b1, b2 = backward_propagation(A0, W1, W2, Z1, A1, A2, b1, b2, np.transpose(Y), len(X), learningRate)
-        #print(A2)
-        # print("YT", np.transpose(Y))
-        if c > 90:
-            print(nextPath, nextTarget)
-            print("A0")
-            print(A0)        
-            print("A1")
-            print(np.around(A1, 3))
-            print("A2")
-            print(np.around(A2, 3), A2[-1:-1])
-            print("W1")
-            print(W1)
-            print("W2")
-            print(W2.shape)
-            print("b")
-            print(b1,b2)
+        # if c > 90:
+        #     print(nextPath, nextTarget)
+        #     print("A0")
+        #     print(A0)        
+        #     print("A1")
+        #     print(np.around(A1, 3))
+        #     print("A2")
+        #     print(np.around(A2, 3))
+        #     print("W1")
+        #     print(np.around(W1, 4))
+        #     print("W2")
+        #     print(np.around(W2, 4))
+        #     print("b")
+        #     print(b1,b2)
             # print("Weights",np.max(W1), np.max(W2))
             # print("b avgs",np.average(b1),np.average(b2))
             # print(b1,b2)
             # pop another item off the image stack and attempt to process it too
-        try:
-            # if c > 10:
-            #     nextPath = originalTrainingPaths.pop()
-                        
-            # else:
+        if len(trainingPaths) > 0:
             nextPath = trainingPaths.pop()
-        except IndexError:
-            nextPath = ''
-        # nextTarget = get_parent_path(nextPath)
-        # nextDataset = path_to_pixels()
+        else:
+            nextPath = '' # originalTrainingPaths.pop()
+           
+    print("c:", c, A0.shape)
+    # testing runs
+    
+    for x in range(10):
+        p = testingPaths.pop()
+        nextTarget = get_parent_path(p)
+        nextDataset = path_to_pixels(p)/(MAX_INPUT/10) # gives us a range between 0 and 10
+    
+        # p = testingPaths.pop()
+        X.append(nextDataset)
+        Z1, Z2, A1, A2 = forward_propagation(np.transpose(X), W1, W2, b1, b2)
+        A2T = np.transpose(A2)
+        print("------Prediction("+nextTarget+")-------")
+        print("shapes", A2.shape, len(Y), len(Y[len(Y)-1]))
+        # print(np.around(A2, 3))
+        print(np.around(A2T[0],2))
+        print(np.around(A2T[-1],2))
+        YT = np.transpose(Y)
+        print("Y", YT.shape)
+        print(np.around(Y[0],4))
+        print(np.around(Y[-1],4))
+        print("W")
+        print(np.min(W1), np.max(W1))
+        print(np.min(W2), np.max(W2))
+        print("W avgs")
+        print(np.average(W1))
+        print(np.average(W2))
+        print("b")
+        print(np.min(b1), np.max(b1))
+        print(np.min(b2), np.max(b2))
+    # print(A2.shape)
+    # all rows of the last column
+    
+  
+    # print(nextPath, nextTarget)
+    # print("A0")
+    # print(A0)        
+    # print("A1")
+    # print(np.around(A1, 3))
+    # print("A2")
+    # print(np.around(A2, 3))
+    # print("W1")
+    # print(np.around(W1, 4))
+    # print("W2")
+    # print(np.around(W2, 4))
+    # print("b")
+    # print(b1,b2)
+    # print(X,Y,W1,W2,b1,b2,A0,A1,A2,Z1,Z2)
+    # print(originalTrainingPaths)
+    # c = 0
+    # nextPath = originalTrainingPaths.pop()
+    # while c < 10:
+    #     c += 1
+    #     nextTarget = get_parent_path(nextPath)
+    #     nextDataset = path_to_pixels(nextPath)
+
+    #     # Y is subtracted from the final probabilities
+    #     # What we're solving for is Y hat or the Y vector of certainy (probability)
+    #     Y.append(fill_at(int(nextTarget)))
+    #     X.append(nextDataset)
+    
+    #     X,Y,W1,W2,b1,b2,A0,A1,A2,Z1,Z2 = there_and_back_again(X,Y,W1,W2,b1,b2,learningRate)
+         
+    #     nextPath = originalTrainingPaths.pop()
+        # .001 every 100 can adjust a whole cycle,  .1 = every 10, .2 every 5
+        # ,A0,A1,A2,Z1,Z2,
+    
+def there_and_back_again(X,Y,W1,W2,b1,b2, learningRate):
+  # number is the layer. 0 = input, 1 = hidden, 2 = output
+        # A is the input set, b is the bias
+        # W are weights that are modified by the forward and backward propagation
+        # print()
+    A0 = np.transpose(X) # m x 784 -> 784 x m
+
+
+    # train the system
+    # A2 is a list of probabilities, 10 total, one for each digit. These total up to 1.
+    Z1, Z2, A1, A2 = forward_propagation(A0, W1, W2, b1, b2)
+    
+    
+    # take what we learned from the training and apply it to our weights so we can learn
+    # this is using a gradient descent (partial differential equations)
+    W1, W2, b1, b2 = backward_propagation(A0, W1, W2, Z1, A1, A2, b1, b2, np.transpose(Y), len(X), learningRate)
+    #print(A2)
+    # print("YT", np.transpose(Y))
+    
+    return (X,Y,W1,W2,b1,b2, A0, A1, A2, Z1, Z2)
 
 def fill_at(y):
      # fake y, which represents the target number    
@@ -135,10 +212,10 @@ def backward_propagation(A0, W1, W2, Z1, A1, A2, b1, b2, Y, m, learningRate):
 
     return (W1,W2,db1,db2)
 
-# non-linear activation function
+# activation function
 def softmax(V):
-    
-    eV = np.exp(V)
+    Vm = V-np.max(V)
+    eV = np.exp(Vm)
     # eV = np.exp(V)
     return eV / np.sum(eV, axis=0)
     
